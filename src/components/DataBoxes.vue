c3b83f333c1d17daa9a2127ccecf5febd47e93ee
Ryan <tayryan0@gmail.com>
2022-12-26 14:32:04 +0800

    a

--- src/components/DataBoxes.vue
+++ src/components/DataBoxes.vue
@@ -2,7 +2,7 @@
   <div class="grid md:grid-cols-2 gap-4">
     <!-- Box 1 -->
     <div class="shadow-md bg-blue-100 p-10 text-center rounded">
-      <h3 class="text-3xl text-blue-900 font-bold mb-4">Cases</h3>
+      <h3 class="text-3xl text-blue-900 font-bold mb-4">Number Of Cases</h3>
 
       <div class="text-2xl mb-4">
         <span class="font-bold">New:</span>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
-  <header class="text-center bg-blue-800 text-white p-4 mb-10">
+  <header class="text-center bg-blue-600 text-white p-4 mb-10">
     <div class="text-3xl md:text-5xl font-bold mb-3">
-      <i class="fa fa-viruses"></i> Covid-19 Tracker
+      Covid-19 Statistics
     </div>
 
     <p>
